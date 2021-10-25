efa4b60e1879875daaed2c5a2fa3b1b59245eb02
Kasya Team <70856891+kasyateam@users.noreply.github.com>
2021-10-25 14:59:27 +0600

    Update kasya_UI.js

--- k.feed/kasya_UI.js
+++ k.feed/kasya_UI.js
@@ -1,7 +1,17 @@
 
 if (window.location.href == 'https://m.kteam.tk/kf-1.html'){
- 
+ document.body.innerHTML = 'Новости'
+}else if (window.location.href == 'https://m.kteam.tk/kf-2.html'){
+ document.body.innerHTML = 'Курсы программирования'
+}else if (window.location.href == 'https://m.kteam.tk/kf-3.html'){
+ document.body.innerHTML = 'Kasya ID'
+}else if (window.location.href == 'https://m.kteam.tk/kf-4.html'){
+ document.body.innerHTML = 'Поддержка'
+}else {
+ window.location.href = 'index.html'
 }
+
+
 
 
 
