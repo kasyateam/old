31d49b576b823c5967659bbc05d959f1d4d6f170
Kasya Team <70856891+kasyateam@users.noreply.github.com>
2021-10-25 19:10:31 +0600

    Update kasya_UI.js

--- k.feed/kasya_UI.js
+++ k.feed/kasya_UI.js
@@ -1,11 +1,11 @@
 
 function ok() {
-    document.body.innerHTML = '<main id="main" onload="ok()"><div id="load"><div id="load_1"><img id="load_2" src="load.gif"></div></div><div id="head"></div><div id="post"></div><hr id="hr"></main>'
+    document.body.innerHTML = '<main id="main" onload="ok()"><div id="load"><div id="load_1"><img id="load_2" src="load.gif"></div></div><div id="head"></div><div id="menu"></div><hr id="hr"></main>'
     
     
     setTimeout(document.getElementById('load').remove(), 5000)
 if (window.location.href == 'https://m.kteam.tk/kf-1.html'){
-document.getElement('post').innerHTML = '<div id="head">Курсы</div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс HTML</p></div><a id="button" href="https://kasyateam.github.io/m/html/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс CSS</p></div><a id="button" href="https://kasyateam.github.io/m/css/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс JavaScript</p></div><a id="button" href="https://kasyateam.github.io/m/js/start.html">Перейти</a></div>'
+ document.getElement('menu').innerHTML = '<div id="head">Курсы</div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс HTML</p></div><a id="button" href="https://kasyateam.github.io/m/html/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс CSS</p></div><a id="button" href="https://kasyateam.github.io/m/css/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс JavaScript</p></div><a id="button" href="https://kasyateam.github.io/m/js/start.html">Перейти</a></div>'
  document.getElement('head').innerHTML = 'Курсы программирования'
 
  
